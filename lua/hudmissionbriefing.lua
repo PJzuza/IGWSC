c56063cf963d784df986d29836d03bf3bdda6426
xtakku <takku@live.de>
2017-09-17 21:22:34 +0200

    futureproofing

--- lua/hudmissionbriefing.lua
+++ lua/hudmissionbriefing.lua
@@ -1,7 +1,10 @@
 IGWSC:Load()
 
+local ready_old = HUDMissionBriefing.set_slot_ready
+local unready_old = HUDMissionBriefing.set_slot_not_ready
+
 function HUDMissionBriefing:set_slot_ready(peer, peer_id)
-	print("set_slot_ready( peer, peer_id )", peer, peer_id)
+	ready_old(self, peer, peer_id)
 	local slot = self._ready_slot_panel:child("slot_" .. tostring(peer_id))
 	if not slot or not alive(slot) then
 		return
@@ -11,7 +14,7 @@ function HUDMissionBriefing:set_slot_ready(peer, peer_id)
 end
 
 function HUDMissionBriefing:set_slot_not_ready(peer, peer_id)
-	print("set_slot_not_ready( peer, peer_id )", peer, peer_id)
+	unready_old(self, peer, peer_id)
 	local slot = self._ready_slot_panel:child("slot_" .. tostring(peer_id))
 	if not slot or not alive(slot) then
 		return
