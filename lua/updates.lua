f5ed3b74c49534eddbcf7ef88ab262b42e6c7c3c
shinrax2 <shinra.ubu1@yahoo.de>
2018-10-17 18:45:49 +0200

    fix auto-update for real this time

--- lua/updates.lua
+++ lua/updates.lua
@@ -2,7 +2,9 @@ if UpdateThisMod then
 	UpdateThisMod:Add({
 		mod_id = 'InGameWaitingStatusColor',
 		data = {
-			modworkshop_id = 19670
+			modworkshop_id = 19670,
+			dl_url = "https://github.com/PJzuza/IGWSC/raw/master/updates/IGWSC.zip",
+			info_url = "https://raw.githubusercontent.com/PJzuza/IGWSC/master/mod.txt"
 		}
 	})
 end
