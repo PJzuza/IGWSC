69d9a01003e3cd171973b2f7f7d81b007437b10a
shinrax2 <shinra.ubu1@yahoo.de>
2018-10-17 15:56:35 +0200

    fix auto-update

--- lua/updates.lua
+++ lua/updates.lua
@@ -1,18 +1,8 @@
-local superblt = false
-for _, mod in pairs(BLT and BLT.Mods:Mods() or {}) do
-	if mod:GetName() == "SuperBLT" and mod:IsEnabled() then
-		superblt = true
-		break
-	end			
-end
-if superblt == false then
-	if UpdateThisMod then
-		UpdateThisMod:Add({
-			mod_id = 'InGameWaitingStatusColor',
-			data = {
-				modworkshop_id = 19670
-			}
-		})
-	end
-
+if UpdateThisMod then
+	UpdateThisMod:Add({
+		mod_id = 'InGameWaitingStatusColor',
+		data = {
+			modworkshop_id = 19670
+		}
+	})
 end
